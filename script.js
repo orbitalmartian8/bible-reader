800786d63c0150c6b748177b4ce2999923af726d
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-06-12 18:46:18 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function loadBook(bookName) {
+  // Replace this with your code to load the content of the selected book and chapter
+  var chapterContent = "This is the content of " + bookName + ".";
+  document.getElementById("chapter-content").innerHTML = chapterContent;
+}
