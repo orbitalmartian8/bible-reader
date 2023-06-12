999cd5b7ebe306b81d06ed2d1bc3673ed0861c9e
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-06-12 18:53:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 function loadBook(bookName) {
-  fetch('bible.json')
-    .then(response => response.json())
-    .then(data => {
-      const bookContent = data[bookName];
-      if (bookContent) {
-        const chapterContent = Object.values(bookContent).join('<br><br>');
-        document.getElementById('chapter-content').innerHTML = chapterContent;
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+  let chapterContent;
+
+  switch (bookName) {
+    case 'Genesis':
+      chapterContent = `
+        <h3>Chapter 1</h3>
+        <p>In the beginning, God created the heavens and the earth...</p>
+        
+        <h3>Chapter 2</h3>
+        <p>Thus the heavens and the earth were finished...</p>
+        
+        <!-- Add more chapters here -->
+      `;
+      break;
+
+    case 'Exodus':
+      chapterContent = `
+        <h3>Chapter 1</h3>
+        <p>These are the names of the sons of Israel who came...</p>
+        
+        <h3>Chapter 2</h3>
+        <p>Now a man from the house of Levi went and took as his wife...</p>
+        
+        <!-- Add more chapters here -->
+      `;
+      break;
+
+    // Add more book cases here
+
+    default:
+      chapterContent = '<p>Book content not available.</p>';
+  }
+
+  document.getElementById('chapter-content').innerHTML = chapterContent;
 }
